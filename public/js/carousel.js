54be1fdf342551c225720ab028bdfc0ad6851858
AdityaNPatil <adityapatil2708@gmail.com>
2023-12-30 00:25:36 +0530

    changed class name of carousel

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,20 +1,20 @@
 // All variables
-const slidesContainer = document.getElementById("slides-container");
-const slide = document.querySelector(".slide");
-const prevButton = document.getElementById("slide-arrow-prev");
-const nextButton = document.getElementById("slide-arrow-next");
+const carouselContainer = document.getElementById("carousel-container");
+const slide = document.querySelector(".carousel-slide");
+const prevButton = document.getElementById("carousel-arrow-prev");
+const nextButton = document.getElementById("carousel-arrow-next");
 
 var count = 1;
 
 // 📁 Scroll to left by width of 1 slide
 nextButton.addEventListener("click", (event) => {
     const slideWidth = slide.clientWidth;
-    slidesContainer.scrollLeft += slideWidth;
+    carouselContainer.scrollLeft += slideWidth;
 
     // if end reached go back to 1st
     count++;
     if (count > 4) {
-        slidesContainer.scrollLeft -= (slideWidth * 4);
+        carouselContainer.scrollLeft -= (slideWidth * 4);
         count = 1; // leftmost slide
     }
 
@@ -24,12 +24,12 @@ nextButton.addEventListener("click", (event) => {
 // 📁 Scroll to right by width of 1 slide
 prevButton.addEventListener("click", () => {
     const slideWidth = slide.clientWidth;
-    slidesContainer.scrollLeft -= slideWidth;
+    carouselContainer.scrollLeft -= slideWidth;
 
     // if we reach left end go to rightmost slide
     count--;
     if (count <1) {
-        slidesContainer.scrollLeft += (slideWidth * 4);
+        carouselContainer.scrollLeft += (slideWidth * 4);
         count = 4;  //rightmost slide
     }
 
